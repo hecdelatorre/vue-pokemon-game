acaacbe01045b60bec91b328e06856fab909d3f6
hecdelatorre <hector982015@gmail.com>
2023-01-20 11:49:28 -0600

    20-01-2023 11:49:28 Diseño de los componentes

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,5 +1,10 @@
 <template>
-  <h3>Pokemon picture</h3>
+  <div class="pokemon-container">
+    <img class="hidden-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
+    <!--
+    <img class="fade-in" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
+    -->
+  </div>
 </template>
 
 <script>
@@ -8,6 +13,23 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.pokemon-container {
+    height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  width: 100vw;
+  margin-left: -50vw;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+  /** pointer-events: none; **/
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
 </style>
