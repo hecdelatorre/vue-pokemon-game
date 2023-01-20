f329dca4e551860c6b5f4e58b40aa67b05141ced
hecdelatorre <hector982015@gmail.com>
2023-01-20 15:36:41 -0600

    20-01-2023 15:36:41

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,10 +1,8 @@
 <template>
   <h1>¿Quién es este pokemon?</h1>
 
-  <PokemonPicture :pokemonId="6" :showPokemon="true"/>
-  <!--
+  <PokemonPicture :pokemonId="6" :showPokemon="false"/>
   <PokemonOptions/>
-   -->
   <!-- TODO: img -->
   <!-- TODO: opciones -->
 </template>
