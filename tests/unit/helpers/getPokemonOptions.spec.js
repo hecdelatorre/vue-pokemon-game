6421e79d17dd609c5534e9e180413c2170ace9f7
hecdelatorre <hector982015@gmail.com>
2023-01-23 00:48:45 -0600

    23-01-2023 00:48:45 getPokemonOptions

--- tests/unit/helpers/getPokemonOptions.spec.js
+++ tests/unit/helpers/getPokemonOptions.spec.js
@@ -1,4 +1,4 @@
-import { getPokemons }  from '@/helpers/getPokemonOptions'
+import getPokemonsOptions, { getPokemons, getPokemonsNames }  from '@/helpers/getPokemonOptions'
 
 describe('getPokemonOptions - helpers', () => {
   test('debe regresar un arreglo de numeros', () => {
@@ -8,7 +8,31 @@ describe('getPokemonOptions - helpers', () => {
     expect(pokemons[150]).toBe(151)
   })
   
-  // TODO
-  // test('debe de retornar un arreglo de 4 elementes con nombres', () => {})
-  // test('getPokemonOptions debe retornar un arreglo mezclado', () => {})
+  test('debe de retornar un arreglo de 4 elementes con nombres', async() => {
+
+    const namesPokemons = await getPokemonsNames([1,2,3,4])
+    console.table(namesPokemons)
+
+    expect(namesPokemons).toStrictEqual([
+      { name: 'bulbasaur', id: 1 },
+      { name: 'ivysaur', id: 2 },
+      { name: 'venusaur', id: 3 },
+      { name: 'charmander', id: 4 }
+    ])
+  })
+  
+  test('getPokemonOptions debe retornar un arreglo mezclado', async() => {
+    const pokemons = await getPokemonsOptions()
+    console.table(pokemons)
+
+    expect(pokemons.length).toBe(4)
+
+    expect(pokemons).toEqual([
+      { name: expect.any(String), id: expect.any(Number) },
+      { name: expect.any(String), id: expect.any(Number) },
+      { name: expect.any(String), id: expect.any(Number) },
+      { name: expect.any(String), id: expect.any(Number) }
+    ])
+
+  })
 })
