927399e559ae04f0a636a99bb7fb663f46901369
hecdelatorre <hector982015@gmail.com>
2023-01-25 00:36:42 -0600

    25-01-2023 00:36:42

--- tests/unit/pages/PokemonPages.spec.js
+++ tests/unit/pages/PokemonPages.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount, mount } from '@vue/test-utils'
 import PokemonPage from '@/pages/PokemonPage'
+import { pokemons } from '../mocks/pokemons.mock'
 
 describe('PokemonPage', () => {
   let wrapper
@@ -17,4 +18,42 @@ describe('PokemonPage', () => {
     expect(mixPokemonArraySpy).toHaveBeenCalled()
   })
 
+  test('debe de hacer match con el snapshot cuando cargan los pokemons', () => {
+    //const wrapper = mount(PokemonPage, {
+    const wrapper = shallowMount(PokemonPage, {
+      data() {
+        return {
+          pokemonArr: pokemons,
+          pokemon: pokemons[0],
+          showPokemon: false,
+          showAnswer: false,
+          message: ''
+        }
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('debe de mostrar los componentes de PokemonPictere y PokemonOptions', () => {
+    const wrapper = shallowMount(PokemonPage, {
+      data() {
+        return {
+          pokemonArr: pokemons,
+          pokemon: pokemons[0],
+          showPokemon: true,
+          showAnswer: false,
+          message: ''
+        }
+      }
+    })
+
+  const picture = wrapper.find('pokemon-picture-stub'),
+        options = wrapper.find('pokemon-options-stub')
+  
+  expect(picture.exists()).toBeTruthy()
+  expect(options.exists()).toBe(true)
+  expect(picture.attributes('pokemonid')).toBe('1')
+  expect(options.attributes('pokemons')).toBeTruthy()
+  })
 })
