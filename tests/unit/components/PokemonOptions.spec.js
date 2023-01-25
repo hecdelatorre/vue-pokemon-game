d28615b31397239b8a9e2f47565d5e680681e360
hecdelatorre <hector982015@gmail.com>
2023-01-24 21:13:20 -0600

    24-01-2023 21:13:20

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -11,9 +11,35 @@ describe('PokemonOptions', () => {
       }
     })
   })
+
   test('debe de hacer match con el snapshot', () => {
-    console.log(wrapper.html())
+    // console.log(wrapper.html())
     expect(wrapper.html()).toMatchSnapshot()
     //expect(wrapper.html()).toMatchInlineSnapshot()
   })
+
+  test('debe de mostrar las 4 opciones correctamente', () => {
+    const [pk1, pk2, pk3, pk4] = wrapper.findAll('li')
+    expect(pk1.text()).toBe('bulbasaur')
+    expect(pk2.text()).toBe('ivysaur')
+    expect(pk3.text()).toBe('venusaur')
+    expect(pk4.text()).toBe('charmander')
+  })
+  
+  test('debe de emitir "selection" con sus respectivos parametros al hacer clic', () => {
+    const [pk1, pk2, pk3, pk4] = wrapper.findAll('li')
+    pk1.trigger('click')
+    pk2.trigger('click')
+    pk3.trigger('click')
+    pk4.trigger('click')
+    console.log(wrapper.emitted('selection'))
+    
+    const [ sE1, sE2, sE3, sE4 ] = wrapper.emitted('selection')
+    expect(sE1).toEqual([1])
+    expect(sE2).toEqual([2])
+    expect(sE3).toEqual([3])
+    expect(sE4).toEqual([4])
+    expect(wrapper.emitted('selection').length).toBe(4)
+  })
+
 })
